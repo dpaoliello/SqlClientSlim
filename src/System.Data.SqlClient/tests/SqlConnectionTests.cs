786560b4270d080501e553c8f262e5d2495bde6d
Daniel Paoliello <daniel@meta-sys.info>
2015-10-08 21:44:44 -0700

    Added test project, got tests working with xUnit console runner

---
+++ src/System.Data.SqlClient/tests/SqlConnectionTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace System.Data.SqlClient.Tests
+{
+    public class SqlConnectionTests
+    {
+        [Fact]
+        public void CreateSqlConnectionTest()
+        {
+            var connection = new SqlConnection();
+        }
+    }
+}

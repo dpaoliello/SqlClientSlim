e4bf72ada951fb9f011b208fd36bd83867d04ef8
Daniel Paoliello <daniel@meta-sys.info>
2017-05-14 19:10:12 -0700

    Enable NuGet package creation, add description

--- src/SqlClientSlim/Properties/AssemblyInfo.cs
+++ src/SqlClientSlim/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("System.Data.SqlClient")]
+[assembly: AssemblyProduct("SqlClientSlim")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
